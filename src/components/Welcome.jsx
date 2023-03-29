59b51017b919f24828004f040e4cc06af6433584
VittoriaBasile <vichy.basile@gmail.com>
2023-03-29 16:49:49 +0200

    commit

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,14 @@
+const Welcome = () => (
+  <div className="container d-flex flex-column bg-light my-3">
+    <h1 className="display-4">Benvenuto in EpiBook</h1>
+    <p className="lead text-start">
+      This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or
+      information.
+    </p>
+    <hr />
+    <p className="lead text-start">
+      It uses utility classes for typography and spacing to space content out within the larger container.
+    </p>
+  </div>
+);
+export default Welcome;
